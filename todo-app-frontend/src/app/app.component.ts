3fb8a57ec5aaa4a53f232cd60103ebf5a7534563
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-06-29 19:05:49 +0530

    add logout function to toolbar

--- todo-app-frontend/src/app/app.component.ts
+++ todo-app-frontend/src/app/app.component.ts
@@ -1,18 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from './shared/auth/auth.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-selector: 'app-root',
-  imports: [
-    RouterOutlet,
-    MatIcon,
-    MatToolbar,
-  ],
+  selector: 'app-root',
+  imports: [RouterOutlet, MatIcon, MatToolbar, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'todo-app-frontend';
+  title = 'todo-app';
+
+  authService = inject(AuthService);
+
+  authUser = this.authService.user;
+
+  logOut() {
+    this.authService.logout();
+  }
 }
