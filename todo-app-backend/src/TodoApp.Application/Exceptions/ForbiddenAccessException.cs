2ce529c65897d29d8dc036cea186a7ef925792e3
Darshana Nuwan Hettiarachchi <dnhnuwan@gmail.com>
2025-06-29 22:07:22 +0530

    Add forbidden access custom exception.

---
+++ todo-app-backend/src/TodoApp.Application/Exceptions/ForbiddenAccessException.cs
@@ -0,0 +1,19 @@
+﻿namespace TodoApp.Application.Exceptions;
+
+public class ForbiddenAccessException : Exception
+{
+    public ForbiddenAccessException()
+        : base("You are not authorized to access this resource.")
+    {
+    }
+
+    public ForbiddenAccessException(string message)
+        : base(message)
+    {
+    }
+
+    public ForbiddenAccessException(string message, Exception innerException)
+        : base(message, innerException)
+    {
+    }
+}
